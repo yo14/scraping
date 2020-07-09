50a364ce32ba83e4153484d88b6eecae0e5ba31b
yo14 <ifliandry11@gmail.com>
2020-07-09 23:22:22 +0700

    Getting value from html tag in html structure

--- fundamentals/app.py
+++ fundamentals/app.py
@@ -2,4 +2,19 @@ from lxml import etree
 
 tree = etree.parse("src/web_page.html")
 
-print(etree.tostring(tree))
+title_element = tree.find("head/title")
+print(title_element.text)
+
+paragraph_element = tree.find("body/p")
+print(paragraph_element.text)
+
+list_items = tree.findall("body/ul/li")
+print(list_items)
+
+for li in list_items:
+    a = li.find("a")
+    if a is not None:
+        print(f"{li.text.strip()} {a.text}")
+    else:
+        print(li.text)
+
