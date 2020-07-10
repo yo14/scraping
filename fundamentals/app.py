4032b78942815c15451ab998e2c632da2f1963d1
yo14 <ifliandry11@gmail.com>
2020-07-10 08:03:14 +0700

    Introduction using lxml and cssselect

--- fundamentals/app.py
+++ fundamentals/app.py
@@ -1,16 +1,24 @@
 from lxml import etree
 
 tree = etree.parse("src/web_page.html")
+html = tree.getroot()
 
-title_element = tree.xpath("//title/text()")[0]
-print(title_element)
+title_element = html.cssselect("title")[0]
+print(title_element.text)
 
-paragraph_element = tree.xpath("//p/text()")[0]
-print(paragraph_element)
+paragraph_element = html.cssselect("p")[0]
+print(paragraph_element.text)
 
-list_items = tree.xpath("//li")
+list_items = html.cssselect("li")
 for li in list_items:
-    text = ''.join(map(str.strip, li.xpath(".//text()")))
-    print(text)
+    a = li.cssselect('a')
+    if len(a) != 0:
+        print(f'{li.text.strip()} {a[0].text}')
+    else:
+        print(li.text)
+
+
+
+
 
 
