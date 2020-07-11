cc8143d82feb69e792823a4b1e39915708b0f504
yo14 <ifliandry11@gmail.com>
2020-07-11 14:25:22 +0700

    Cleaning data using regex or lambda

--- projectone/app.py
+++ projectone/app.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+import re
 
 resp = requests.get(url="http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
                     headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'})
@@ -9,13 +10,15 @@ tree = html.fromstring(html=resp.text)
 product_main = tree.xpath("//div[contains(@class,'product_main')]")[0]
 title = product_main.xpath(".//h1/text()")[0]
 price = product_main.xpath(".//p[1]/text()")[0]
-availibility = product_main.xpath(".//p[2]/text()")[1].strip()
+availability = product_main.xpath(".//p[2]/text()")[1].strip()
+# in_stock = re.compile(r"\d+").findall(availability)[0] # cleaning with regex
+in_stock = ''.join(list(filter(lambda x: x.isdigit(), availability)))
 description = tree.xpath("//div[@id='product_description']/following-sibling::p/text()")[0]
 
 book_information = {
     'title':title,
     'price':price,
-    'in_stock': availibility,
+    'in_stock': in_stock,
     'description': description
 }
 
