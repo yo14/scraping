b089b4edef2c119b1c4cbbd38ca0c6f05b8bfc02
yo14 <ifliandry11@gmail.com>
2020-07-11 08:51:16 +0700

    Building the scraper

--- projectone/app.py
+++ projectone/app.py
@@ -1,7 +1,25 @@
 import requests
+from lxml import html
 
-resp = requests.get(url="http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'})
+resp = requests.get(url="http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
+                    headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'})
 
-print(resp.headers)
-print('-' * 20)
-print(resp.request.headers)
+tree = html.fromstring(html=resp.text)
+
+product_main = tree.xpath("//div[contains(@class,'product_main')]")[0]
+title = product_main.xpath(".//h1/text()")[0]
+price = product_main.xpath(".//p[1]/text()")[0]
+availibility = product_main.xpath(".//p[2]/text()")[1].strip()
+description = tree.xpath("//div[@id='product_description']/following-sibling::p/text()")[0]
+
+book_information = {
+    'title':title,
+    'price':price,
+    'in_stock': availibility,
+    'description': description
+}
+
+print(book_information)
+
+
+
